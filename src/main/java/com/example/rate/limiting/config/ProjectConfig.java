7487a3bd649f5ad0e28a126a7561cb24c252591e
STARK <admiraldapo@gmail.com>
2026-08-02 19:02:02 +0100

    bucket4j impl

--- src/main/java/com/example/rate/limiting/config/ProjectConfig.java
+++ src/main/java/com/example/rate/limiting/config/ProjectConfig.java
@@ -1,5 +1,10 @@
 package com.example.rate.limiting.config;
 
+import io.github.bucket4j.distributed.ExpirationAfterWriteStrategy;
+import io.github.bucket4j.distributed.proxy.ProxyManager;
+import io.github.bucket4j.distributed.serialization.Mapper;
+import io.github.bucket4j.redis.jedis.Bucket4jJedis;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
@@ -11,13 +16,23 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
+import java.time.Duration;
 import java.util.concurrent.Executor;
 
 @Configuration
 @EnableAsync
 @EnableScheduling
+@SuppressWarnings({"deprecation"})
 public class ProjectConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
 
     @Bean(name = "taskExecutor")
     public Executor taskExecutor() {
@@ -53,5 +68,30 @@ public class ProjectConfig {
         );
         script.setResultType(Long.class);
         return script;
+    }
+
+    @Bean
+    public JedisPool jedisPool() {
+        JedisPoolConfig poolConfig = new JedisPoolConfig();
+
+        poolConfig.setMaxTotal(50);
+        poolConfig.setMaxIdle(10);
+        poolConfig.setMinIdle(5);
+
+        poolConfig.setTestOnBorrow(true);
+        poolConfig.setTestWhileIdle(true);
+        poolConfig.setBlockWhenExhausted(true);
+
+        return new JedisPool(poolConfig, redisHost, redisPort, 30000, null);
+    }
+
+    @Bean
+    public ProxyManager<String> proxyManager(JedisPool jedisPool) {
+        return Bucket4jJedis.casBasedBuilder(jedisPool)
+                .keyMapper(Mapper.STRING)
+                .expirationAfterWrite(
+                        ExpirationAfterWriteStrategy.basedOnTimeForRefillingBucketUpToMax(Duration.ofMinutes(2))
+                )
+                .build();
     }
 }
